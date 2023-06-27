963e87f0cad3d6eebfae2b5a5a89d5303ad9a308
usuario <usuario@DESKTOP-NKKQGOJ>
2023-06-27 14:17:41 -0500

    Agregar archivos de proyecto.

---
+++ Punto1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Punto1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTempEnt = new System.Windows.Forms.TextBox();
+            this.cbTipTempEnt = new System.Windows.Forms.ComboBox();
+            this.cbTipoTempSal = new System.Windows.Forms.ComboBox();
+            this.lbTempSal = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnTerminar = new System.Windows.Forms.Button();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(212, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Conversor de temperatura";
+            // 
+            // txtTempEnt
+            // 
+            this.txtTempEnt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTempEnt.Location = new System.Drawing.Point(216, 106);
+            this.txtTempEnt.Name = "txtTempEnt";
+            this.txtTempEnt.Size = new System.Drawing.Size(98, 22);
+            this.txtTempEnt.TabIndex = 1;
+            // 
+            // cbTipTempEnt
+            // 
+            this.cbTipTempEnt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTipTempEnt.FormattingEnabled = true;
+            this.cbTipTempEnt.Items.AddRange(new object[] {
+            "Celsius",
+            "Fahrenheit",
+            "Kelvin",
+            "Rankine"});
+            this.cbTipTempEnt.Location = new System.Drawing.Point(193, 182);
+            this.cbTipTempEnt.Name = "cbTipTempEnt";
+            this.cbTipTempEnt.Size = new System.Drawing.Size(121, 26);
+            this.cbTipTempEnt.TabIndex = 2;
+            // 
+            // cbTipoTempSal
+            // 
+            this.cbTipoTempSal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTipoTempSal.FormattingEnabled = true;
+            this.cbTipoTempSal.Items.AddRange(new object[] {
+            "Celsius",
+            "Fahrenheit",
+            "Kelvin",
+            "Rankine"});
+            this.cbTipoTempSal.Location = new System.Drawing.Point(392, 182);
+            this.cbTipoTempSal.Name = "cbTipoTempSal";
+            this.cbTipoTempSal.Size = new System.Drawing.Size(121, 26);
+            this.cbTipoTempSal.TabIndex = 3;
+            // 
+            // lbTempSal
+            // 
+            this.lbTempSal.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbTempSal.Location = new System.Drawing.Point(389, 102);
+            this.lbTempSal.Name = "lbTempSal";
+            this.lbTempSal.Size = new System.Drawing.Size(100, 26);
+            this.lbTempSal.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gainsboro;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ingrese la magnitud";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Gainsboro;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ingrese el tipo";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(113, 262);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(88, 35);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnTerminar
+            // 
+            this.btnTerminar.Location = new System.Drawing.Point(464, 262);
+            this.btnTerminar.Name = "btnTerminar";
+            this.btnTerminar.Size = new System.Drawing.Size(88, 35);
+            this.btnTerminar.TabIndex = 8;
+            this.btnTerminar.Text = "Terminar";
+            this.btnTerminar.UseVisualStyleBackColor = true;
+            this.btnTerminar.Click += new System.EventHandler(this.btnTerminar_Click);
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(293, 262);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(88, 35);
+            this.btnConvertir.TabIndex = 9;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 321);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.btnTerminar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbTempSal);
+            this.Controls.Add(this.cbTipoTempSal);
+            this.Controls.Add(this.cbTipTempEnt);
+            this.Controls.Add(this.txtTempEnt);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTempEnt;
+        private System.Windows.Forms.ComboBox cbTipTempEnt;
+        private System.Windows.Forms.ComboBox cbTipoTempSal;
+        private System.Windows.Forms.Label lbTempSal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnTerminar;
+        private System.Windows.Forms.Button btnConvertir;
+    }
+}
+
---
+++ Punto1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using libConversor;
+
+
+namespace Punto1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTerminar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {       
+            this.txtTempEnt.Text = String.Empty;
+            this.lbTempSal.Text = String.Empty;
+            this.cbTipTempEnt.Text = String.Empty;
+            this.cbTipoTempSal.Text = String.Empty;
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            float fltTempEnt;
+            int intTipTempEnt, intTipTempSal;
+            try
+            {
+                fltTempEnt = Convert.ToSingle(this.txtTempEnt.Text);
+                intTipTempEnt = cbTipTempEnt.SelectedIndex;
+                intTipTempSal = cbTipoTempSal.SelectedIndex;
+
+                clsConversor oC = new clsConversor();
+
+                oC.tempEnt = fltTempEnt;
+                oC.opcion = intTipTempEnt;
+                oC.tipTempSal = intTipTempSal;
+
+
+                // TRATAMIENTO DEL ERROR
+
+                if (!oC.Convertir())
+                {
+                    MessageBox.Show(oC.Error);
+                    oC = null; // opcional
+                    return;
+                }
+
+                //Mostrar info
+
+                this.lbTempSal.Text = oC.tempSal.ToString();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
