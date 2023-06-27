47c60771800a0adf01a3062c7184bf5094fd8b96
JUAN FELIPE USUGA ARISTIZABAL <juanusuga290715@correo.itm.edu.co>
2023-06-27 17:11:12 -0500

    Se movio un label

--- Punto1/Form1.Designer.cs
+++ Punto1/Form1.Designer.cs
@@ -111,7 +111,7 @@ namespace Punto1
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Gainsboro;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 187);
+            this.label3.Location = new System.Drawing.Point(49, 187);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(107, 16);
             this.label3.TabIndex = 6;
